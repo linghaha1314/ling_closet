8f87745fef143d3913049f877decc80456b054b7
Perperon <du1443528055@163.com>
2024-11-11 09:29:32 +0800

    增加导出导入功能

--- clothing-wardrobe/src/app/wardrobe/wardrobe.page.ts
+++ clothing-wardrobe/src/app/wardrobe/wardrobe.page.ts
@@ -1,9 +1,10 @@
 // src/app/wardrobe/wardrobe.page.ts
-import { Component, OnInit } from '@angular/core';
-import { DexieService, ClothingItem } from '../DexieService';
-import { ModalController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {ClothingItem, DexieService} from '../DexieService';
+import {ModalController} from '@ionic/angular';
 import {AddClothingItemComponent} from "../components/add-clothing-item/add-clothing-item.component";
-import { EditClothingItemComponent } from '../components/edit-clothing-item/edit-clothing-item.component';
+import {EditClothingItemComponent} from '../components/edit-clothing-item/edit-clothing-item.component';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-wardrobe',
@@ -12,7 +13,9 @@ import { EditClothingItemComponent } from '../components/edit-clothing-item/edit
 })
 export class WardrobePage implements OnInit {
   clothingItems: ClothingItem[] = [];
-  defaultImage = 'assets/icon/logo.png'
+  upData: ClothingItem[] = [];
+  defaultImage = 'assets/icon/logo.png';
+  isModalOpen = false;
   constructor(
     private dexieService: DexieService,
     private modalController: ModalController
@@ -54,5 +57,54 @@ export class WardrobePage implements OnInit {
   onImageError(event: Event) {
     const imgElement = event.target as HTMLImageElement;
     imgElement.src = this.defaultImage; // 设置默认图片路径
+  }
+
+  // 导出为 Excel 文件
+  exportToExcel() {
+    // 创建工作表
+    const worksheet = XLSX.utils.json_to_sheet(this.clothingItems);
+    // 创建工作簿并将工作表添加到其中
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'ClothingItems');
+    // 导出为 Excel 文件
+    XLSX.writeFile(workbook, 'wardrobe.xlsx');
+  }
+  // 从 Excel 文件导入数据
+  onFileChange(event: any) {
+    const target: DataTransfer = <DataTransfer>(event.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      // 获取第一个工作表
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      // 将工作表的数据转换为 JSON
+      this.upData = <ClothingItem[]>XLSX.utils.sheet_to_json(ws)
+      console.log(this.upData,888)
+      this.isModalOpen = true
+      // 保存到数据库
+      // this.saveImportedData(data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  // 保存导入的数据到 Dexie 数据库
+  async saveImportedData() {
+    const resetButton = document.getElementById('resetButton');
+    for (const item of this.upData) {
+      await this.dexieService.addClothingItem(item);
+    }
+    this.clothingItems = await this.dexieService.getAllClothingItems();
+    this.isModalOpen = false
+    resetButton?.click();
+  }
+
+  onDidDismiss(event: CustomEvent) {
+    console.log('didDismiss', JSON.stringify(event.detail.value));
   }
 }
